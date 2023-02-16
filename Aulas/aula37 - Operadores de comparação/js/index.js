828ed4eba10ed0c56cc1ddfc5d4edaff0aa389f2
mynameisfarelo <viniciussieben7@gmail.com>
2023-02-16 15:20:33 -0300

    Adicionado aula sobre operadores de comparação

---
+++ Aulas/aula37 - Operadores de comparação/js/index.js
@@ -0,0 +1,25 @@
+/* 
+
+Operadores de comparação
+
+> Maior que
+>= Maior que ou igual a
+< Menor que
+<= Menor que ou igual a
+== Igualdade (checa valor)
+=== Igualdade estrita (checa valor e tipo )
+!= Diferente
+!== Diferente estrito
+
+*/
+
+const comparacao = 10 > 5;
+console.log(comparacao);
+
+console.log(10 >= 10);
+console.log(0 < 1);
+console.log(1 <= 2);
+console.log(7 == 7);
+console.log(7 === '7');
+console.log(-240 != 240);
+console.log('100' !== 100);
